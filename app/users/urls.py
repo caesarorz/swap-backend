b8733976a60e2d1dbb7ed7b19f7f0e914f90fb21
Cesar Orozco <caesar.orz@gmail.com>
2024-03-08 19:56:45 -0600

    Add urls, serializer and views for seen user

--- app/users/urls.py
+++ app/users/urls.py
@@ -3,11 +3,12 @@ Users urls
 """
 
 from django.urls import path
-from users.views import CreateUserView, AuthTokenView
+from users.views import CreateUserView, AuthTokenView, ManageUserView
 
 app_name = 'users'
 
 urlpatterns = [
     path('add/', CreateUserView.as_view(), name='add'),
     path('authenticate/', AuthTokenView.as_view(), name='auth'),
+    path('detail/', ManageUserView.as_view(), name='detail'),
 ]

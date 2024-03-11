58095416813eaa190ff19e5dd468438744efe4eb
Cesar Orozco <caesar.orz@gmail.com>
2024-03-11 10:51:52 -0600

    Enable JWT token

--- app/users/urls.py
+++ app/users/urls.py
@@ -3,11 +3,19 @@ Users urls
 """
 
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 from users.views import CreateUserView, AuthTokenView, ManageUserView
 
 app_name = 'users'
 
 urlpatterns = [
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('add/', CreateUserView.as_view(), name='add'),
     path('authenticate/', AuthTokenView.as_view(), name='auth'),
     path('detail/', ManageUserView.as_view(), name='detail'),

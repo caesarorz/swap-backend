aced20f27f28f12b4c57a0a6cbd4dd2e3ab650ac
Cesar Orozco <caesar.orz@gmail.com>
2024-03-13 10:49:55 -0600

    Clean payments serializers

--- app/payments/serializers.py
+++ app/payments/serializers.py
@@ -8,19 +8,4 @@ class PaymentMethodSerializer(serializers.ModelSerializer):
     class Meta:
         model = PaymentMethod
         fields = '__all__'
-        # extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
-    # def create(self, validated_data):
-    #     """Create and return a user with encrypted password."""
-    #     return PaymentMethod.objects.create_user(**validated_data)
-
-    # def update(self, instance, validated_data):
-    #     """Update and return user."""
-    #     password = validated_data.pop('password', None)
-    #     user = super().update(instance, validated_data)
-
-    #     if password:
-    #         user.set_password(password)
-    #         user.save()
-
-    #     return user

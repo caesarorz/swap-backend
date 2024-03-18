dee20d341a45f1e26dbf5090cba5979a552a5b56
Cesar Orozco <caesar.orz@gmail.com>
2024-03-18 13:02:50 -0600

    Proxy server

--- app/offers/views.py
+++ app/offers/views.py
@@ -22,3 +22,33 @@ class OfferViewSet(mixins.CreateModelMixin,
     serializer_class = OfferSerializer
     queryset = Offer.objects.all()
     authentication_classes = [JWTAuthentication]
+
+
+class OffersMatchView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    serializer_class = OfferSerializer
+    queryset = Offer.objects.all()
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        """Retrive payment methods for authenticated users"""
+        print("get_queryset", self.request.META)
+        # if self.request.user.is_staff:
+        return self.queryset.all().order_by('-id')
+        # return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Set serializer for actions detail or list"""
+        print("get_serializer_class", self.request.data)
+
+        # print(self.queryset.filter(user_payment_method_id=))
+
+        # if self.action == 'list':
+        #     return OfferSerializer
+        return self.serializer_class
+
+    # def get(self, request, format=None):
+    #     """Get user payments"""
+    #     print("get", request)
+    #     user_payments = Offer.objects.all()
+    #     serializer = OfferSerializer(user_payments, many=True)
+    #     return Response(serializer.data)
